9f02b23a2a5c159da72d1c27b87603fe95d926ac
ZJBarney <ZJBarney@gmail.com>
2017-01-31 10:32:58 -0700

    Update webpages.h

--- webpages.h
+++ webpages.h
@@ -3,7 +3,8 @@
 
 String file1=
 "<h1 style=\"color: #5e9ca0;\">This is the HOME page!&nbsp;</h1>\r\n"
-"<p>Go to the&nbsp;<a href=\"lightstatus\" target=\"_blank\">Light Status Page</a></p>\r\n"
+"<p>Go to the&nbsp;<a href=\"lightstatus\" target=\"_self\">Light Status Page</a></p>\r\n"
+"<p>Go to the&nbsp;<a href=\"timeoutsetting\" target=\"_self\">Light Timeout Option page</a></p>\r\n"
 "<p><img src=\"https://tuts.codingo.me/wp-content/uploads/2016/05/ESP-12.jpg\" alt=\"Mighty Mighty Chip!\" width=\"308\" height=\"231\" /></p>\r\n";
 
 String file2a=
@@ -13,10 +14,26 @@ String file2b=
 "<table>\r\n"
 "<tbody>\r\n"
 "<tr>\r\n";
-String file2C=
+String file2c=
 "</tr>\r\n"
 "</tbody>\r\n"
 "</table>\r\n"
-"<p><br /><img src=\"https://tuts.codingo.me/wp-content/uploads/2016/05/ESP-12.jpg\" alt=\"Mighty Mighty Chip!\" width=\"308\" height=\"231\" /></p>\r\n";
+"<p>Go to the&nbsp;<a href=\"timeoutsetting\" target=\"_self\">Light Timeout Option page</a></p>\r\n"
+"<p><br /><img src=\"https://tuts.codingo.me/wp-content/uploads/2016/05/ESP-12.jpg\" alt=\"Mighty Mighty Chip!\" width=\"308\" height=\"231\" /></p>\r\n"
+"Timeout counter in 0.5 second interval:  ";
+
+
+String file3=
+"<h1><span style=\"color: #000000;\">Lighting timer delay page</span></h1>\r\n"
+"<p>Enter the amount of time for the lights to stay on after motion being detected:&nbsp;</p>\r\n"
+"<body>"
+"</p><form method='get' action='setting'><label>Delay time in minutes: </label><input name='Time' length=32><input type='submit'></form>"
+"<p>Click the Submit button to confirm time and update motion sensor.</p>"
+"</body>"
+"</html>";
+
+String file4=
+"<h1>Success: Saved new timeout. Return to status to check lights"
+"<p>Go to the <a href=\"lightstatus\" target=\"_self\">Light Status Page</a></p>\r\n";
 
 #endif
