8d6f523922ecab94cba431e0a3834da282fdc2b3
ZJBarney <ZJBarney@gmail.com>
2017-03-26 14:40:50 -0700

    Add files via upload

---
+++ motionsensorwebserverwebconfig/launchWeb.ino
@@ -0,0 +1,12 @@
+void launchWeb(int webtype) {
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("SoftAP IP: ");
+  Serial.println(WiFi.softAPIP());
+  createWebServer(webtype);
+  // Start the server
+  server.begin();
+  Serial.println("Server started"); 
+}
