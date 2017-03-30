10caf53da8642c9c72195d6bc4425a58aedf0070
driazmike <azmike_1@hotmail.com>
2017-03-30 12:37:07 -0700

    Implements DNS support
    broadcast service on port 8080 so clients can find base

--- motionsensorwebserverwebconfig/launchWeb.ino
+++ motionsensorwebserverwebconfig/launchWeb.ino
@@ -10,3 +10,27 @@ void launchWeb(int webtype) {
   server.begin();
   Serial.println("Server started"); 
 }
+
+void startDNS()
+{ 
+  sprintf(hostString, "ESP_%06X", ESP.getChipId());
+  Serial.print("Hostname: ");
+  Serial.println(hostString);
+  WiFi.hostname(hostString);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+ if (!MDNS.begin(hostString)) {
+    Serial.println("Error setting up MDNS responder!");
+  }
+ Serial.println("mDNS responder started");
+ MDNS.addService("esp", "tcp", 8080); // Announce esp tcp service on port 8080
+}
